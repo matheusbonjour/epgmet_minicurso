93d81e37b8e3967989c7b481d6cd1fcb3f0f9776
Matheus Bonjour <matheus.bonjour@gmail.com>
2023-10-19 10:11:30 -0300

    ex1

--- exercicio1.py
+++ exercicio1.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib.gridspec as gridspec
 import cmocean.cm as cmo    
 
+
+#plt.rcParams('font.size') = 14 
+
+
 path = 'dados_epgmet/modis_data/AQUA_MODIS.20221001_20221031.L3m.MO.SST4.sst4.9km.nc'
 path1 = 'dados_epgmet/modis_data/AQUA_MODIS.20221001_20221031.L3m.MO.CHL.chlor_a.9km.nc'
 
@@ -27,10 +31,13 @@ ax1 = fig.add_subplot(gs[0], projection=ccrs.PlateCarree())
 cst = ax1.contourf(lon, lat, ds_tsm.sst4, transform=ccrs.PlateCarree(), cmap=cmo.thermal, levels=levels)
 
 ax1.coastlines()
+
+
 ax1.gridlines()
+
 ax1.set_title('SST MODIS')
 
-plt.colorbar(cst, shrink= 0.7, aspect= 20, label='SST (°C)')
+plt.colorbar(cst, shrink= 0.5, aspect= 20, label='SST (°C)')
 #ax1.colorbar()
 
 ax2 = fig.add_subplot(gs[1], projection=ccrs.PlateCarree())
@@ -50,8 +57,11 @@ ax2.gridlines()
 
 ax2.set_title('CHL MODIS')  
 
-plt.colorbar(csch, shrink= 0.7, aspect= 40, label='CHL (mg/m3)')
+plt.colorbar(csch, shrink= 0.5, aspect= 20, label='CHL (mg/m3)')
 
+gl = ax1.gridlines(crs=ccrs.PlateCarree(), color='gray', alpha=1.0, linestyle='--', linewidth=0.25, xlocs=np.arange(-180, 180, 5), ylocs=np.arange(-90, 90, 5), draw_labels=True)
+gl.xlabel_style = {'fontsize': 14, 'fontweight': 'bold'}
+gl.ylabel_style = {'fontsize': 14, 'fontweight': 'bold'}
 
 plt.show() 
 
